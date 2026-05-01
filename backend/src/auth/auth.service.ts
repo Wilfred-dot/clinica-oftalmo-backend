8cb8a721307d85001aab37efdbf03323b7086188
Wilfred <wilfredjr4@gmail.com>
2026-05-01 15:45:09 +0200

    Autenticação JWT totalmente funcional com login

---
+++ backend/src/auth/auth.service.ts
@@ -0,0 +1,30 @@
+﻿import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(email: string, pass: string): Promise<any> {
+    const user = await this.prisma.users.findUnique({ where: { email } });
+    if (!user) throw new UnauthorizedException('Credenciais inválidas');
+
+    const isMatch = await bcrypt.compare(pass, user.password);
+    if (!isMatch) throw new UnauthorizedException('Credenciais inválidas');
+
+    const { password, ...result } = user;
+    return result;
+  }
+
+  async login(user: any) {
+    const payload = { email: user.email, sub: user.id, role: user.role };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
